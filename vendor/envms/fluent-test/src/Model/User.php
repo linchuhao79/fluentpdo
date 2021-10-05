b39c853e427c898abbedb57341ac45d596fc44ae
Chu Hao Lin <chuhaolin@qookia.cc>
2021-10-05 17:19:14 +0800

    aaaa

---
+++ vendor/envms/fluent-test/src/Model/User.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Envms\FluentTest\Model;
+
+/**
+ * Class User //test????
+ */
+class User {
+
+    /** @var int */
+    public $id;
+
+    /** @var int */
+    public $country_id;
+
+    /** @var string */
+    public $type;
+
+    /** @var string */
+    public $name;
+
+}
